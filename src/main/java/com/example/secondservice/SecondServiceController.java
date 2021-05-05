71804f2e91b10adcc39584f621ab82addaf2da70
jong-geungwon <ngc@imform.co.kr>
2021-05-06 00:15:41 +0900

    eureka 를 위한 포트 및 instance-id 설정 추가

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,22 @@
 package com.example.secondservice;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
+@AllArgsConstructor
 @RequestMapping("/second-service")
 public class SecondServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -23,7 +30,9 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from Second service";
+    public String check(HttpServletRequest request){
+        log.info("server port={}",request.getServerPort());
+        return String.format("Hi, there. This is a message from Second service %s"
+                , env.getProperty("local.server.port"));
     }
 }
